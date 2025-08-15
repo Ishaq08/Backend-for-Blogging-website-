22a9be661ee922e1d7f131a5c35ad45267d5c7e6
ishaq08 <kmishaq08@gmail.com>
2025-08-15 23:53:38 +0500

    update blog model

--- src/models/blog.model.js
+++ src/models/blog.model.js
@@ -2,16 +2,19 @@ import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema(
   {
-        title: {
-          type: String,
-          required: true
-        },
-        content: {
-            type: String,
-            required: true
-        },
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String
+    }, // store Cloudinary URL
   },
   { timestamps: true }
 );
 
-export const Blog = mongoose.model("Blog", blogSchema)
+export const Blog = mongoose.model('Blog', blogSchema);
